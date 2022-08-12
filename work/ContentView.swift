d2384b4b198083378e09aea61e4649dca31381ae
Heyi <al6al60212@gmail.com>
2022-08-12 21:05:38 +0800

    Initial Commit

---
+++ work/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  work
+//
+//  Created by 董禾翊 on 2022/8/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ work/workApp.swift
@@ -0,0 +1,17 @@
+//
+//  workApp.swift
+//  work
+//
+//  Created by 董禾翊 on 2022/8/12.
+//
+
+import SwiftUI
+
+@main
+struct workApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
